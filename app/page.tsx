350c487257816abe461420d6159b3f2a62753e6b
paulprince24542 <paulprince2454@gmail.com>
2024-10-03 12:15:16 +0530

    animation added

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,23 @@ import Navbar from "./components/main/Navbar";
 import Footer from "./components/main/Foot";
 import Events from "./components/main/Events";
 import Pacman from "./components/main/pacman";
+import "./index.css"
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full h-full md:w-full md:h-full items-center justify-between">
-      <Navbar />
+      <Navbar />  
+
 
 
       <div className=" grid w-full px-[30px] py-[30px] h-full mt-[40px] rounded-[20px] p-24 md:gap-[80px] gap-[40px] md:top-[150px] md:left-[270px] md:pt-[80px] pt-[40px] md:pr-[80px] pr-[16px] md:pb-[24px] pb-[16px] md:pl-[80px] pl-[16px]">
-        <div className="w-[full] flex flex-col items-center justify-center mt-[150px]">
+        <div className="flex justify-center smyk-654 ">
+          <video className="w-[1000px] h-[500px]" autoPlay loop muted playsInline src="/bitrat.mp4"></video>
+        </div>
+        <div className="w-[full] flex flex-col items-center justify-center">
           <p className="text-[40px] md:text-[65px] lg:text-[85px] text-[#ff9933] font-[400] main-heading">SAMYUKTHA</p>
-          <p className="text-[40px] md:text-[65px] lg:text-[85px] text-[#ff9933] main-heading">8.0</p>
-    <div className="main-heading">8.0</div>
+          {/* <p className="text-[40px] md:text-[65px] lg:text-[85px] text-[#ff9933] main-heading">8.0</p>
+          <div className="main-heading">8.0</div> */}
         </div>
 
 
