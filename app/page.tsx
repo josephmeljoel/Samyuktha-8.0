4a78c9e67a76b5e4105bc395a64473dcfa370f6c
josephmeljoel <josephmeljoel724@gmail.com>
2024-10-04 16:28:03 +0530

    about us updated

--- app/page.tsx
+++ app/page.tsx
@@ -32,21 +32,27 @@ export default function Home() {
 
 
 
-        <div className="w-full flex flex-col items-center justify-center mt-[300px]">
-          <p className="text-green-400 md:text-[30px] pb-[20px] text-[20px]">About</p>
-          <p className=" text-white md:text-[25px] text-[18px] font-[Inter] pb-5 pt-5">
+        <div className="flex w-full h-full items-center justify-center mt-[300px]">
+          <div className="w-1/2 h-full">
+          <img src="logo-textured.png" alt="" className="w-[600px] h-[600px] items-center justify-center">
+          
+          </img>
+          
+          </div>
+          <div className="w-1/2 h-full">
+          <p className="text-green-400 text-[Sega] md:text-[30px] pb-[20px] text-center text-[20px] ">About</p> 
+          <p className="text-center text-[grey] md:text-[20px] text-[10px] font-[Inter] pb-5 pt-5">
             SAINTGITS College of Engineering organizes ‘Samyuktha 6.0’, a national level techno cultural fest, on
             March 24th. ‘Samyuktha’ has been successfully organized for the past five years and serves as an ideal
             platform for talented undergraduate and postgraduate computer application students to showcase their
-            technical and artistic skills.</p>
-
-          <p className=" text-white md:text-[25px] text-[18px] font-[Inter] pb-5 pt-5">In this fest, students will compete for more than 10 events like Best Singer, Spot Photography, Coding,
-            Web Designing, Treasure Hunt, Spot Dance. A total prize money of Rs.70000 awaits the contestants.</p>
-
-          <p className="text-white md:text-[25px] text-[18px] font-[Inter] pb-5 pt-5">Saintgits College of Engineering launched ‘Samyuktha’ in the year 2016 with the aim of bringing to the
+            technical and artistic skills.
+          In this fest, students will compete for more than 10 events like Best Singer, Spot Photography, Coding,
+            Web Designing, Treasure Hunt, Spot Dance. A total prize money of Rs.70000 awaits the contestants.
+            Saintgits College of Engineering launched ‘Samyuktha’ in the year 2016 with the aim of bringing to the
             forefront the artistic, cultural and technical skills of students amidst their busy academic schedule.
             ‘Samyuktha’ is jointly organized by a team of dedicated staff members and students who are leaving no
             stone unturned to ensure that this year’s ‘Samyuktha’ is bigger and better than the previous year.</p>
+          </div>
 
         </div>
 
@@ -61,3 +67,18 @@ export default function Home() {
     </main>
   );
 }
+
+{/* <p className="text-green-400 md:text-[30px] pb-[20px] text-[20px]">About</p> 
+          <p className=" text-white md:text-[25px] text-[18px] font-[Inter] pb-5 pt-5">
+            SAINTGITS College of Engineering organizes ‘Samyuktha 6.0’, a national level techno cultural fest, on
+            March 24th. ‘Samyuktha’ has been successfully organized for the past five years and serves as an ideal
+            platform for talented undergraduate and postgraduate computer application students to showcase their
+            technical and artistic skills.</p>
+
+          <p className=" text-white md:text-[25px] text-[18px] font-[Inter] pb-5 pt-5">In this fest, students will compete for more than 10 events like Best Singer, Spot Photography, Coding,
+            Web Designing, Treasure Hunt, Spot Dance. A total prize money of Rs.70000 awaits the contestants.</p>
+
+          <p className="text-white md:text-[25px] text-[18px] font-[Inter] pb-5 pt-5">Saintgits College of Engineering launched ‘Samyuktha’ in the year 2016 with the aim of bringing to the
+            forefront the artistic, cultural and technical skills of students amidst their busy academic schedule.
+            ‘Samyuktha’ is jointly organized by a team of dedicated staff members and students who are leaving no
+            stone unturned to ensure that this year’s ‘Samyuktha’ is bigger and better than the previous year.</p> */}
