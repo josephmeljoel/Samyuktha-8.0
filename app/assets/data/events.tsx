0ec9c7c774fb68f38d74ee8ea747c465c42428f3
paulprince24542 <paulprince2454@gmail.com>
2024-10-08 00:35:46 +0530

    transition added

---
+++ app/assets/data/events.tsx
@@ -0,0 +1,206 @@
+'use client';
+var EventData = [
+  {
+    id: 12,
+    url: "/event1.jpg",
+    event_name: "Disco Frenzy",
+    registeration_fee: 50,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Antony Sebastian",
+    head_phonenumber_1: "9496940988",
+    head_name_2: "Lakshmi S Nair",
+    head_phonenumber_2: "9497295391",
+    description:"Get ready to dance your heart out at the most electrifying event of Samyuktha! Spot Dance is a thrilling and unique dance competition that encapsulates the essence of spontaneity, creativity, and the sheer love for dancing.It's your chance to shine, showcase your dancing skills, and be the star of the moment. Participants are given a surprise track, and they must instantly sync their moves to the music. The result is a captivating display of spontaneous choreography and unscripted emotions that thrill both the dancers and the audience. Get ready to be wowed by Spot Dance where rhythm meets spontaneity!"}, 
+    
+    
+    {
+    id: 17,
+    url: "/event2.jpg",
+    event_name: "Flashback Frames",
+    registeration_fee: 50,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Sharon Shiju",
+    head_phonenumber_1: "8139090192",
+    head_name_2: "Albin v Johnson",
+    head_phonenumber_2: "9496161839",
+    description:"The relentless river that flows, untamed by mortal hands. It slips through our fingers, marking our past, present, and future. In its embrace, memories fade, moments are seized, and life's journey unfolds. Explore the ephemeral beauty and unyielding march of time in this mesmerizing 30-second reel."},
+  {
+    id: 12,
+    url: "/event3.jpg",
+    event_name: "Zeus Odyssey",
+    registeration_fee: 100,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Sandeep Suresh",
+    head_phonenumber_1: "9567862431",
+    head_name_2: "Mathew Paul",
+    head_phonenumber_2: "8547423887",
+    description:
+  "hjasu"},
+
+
+  {
+    id: 13,
+    url: "/event4.jpg",
+    event_name: "Neon Nexus",
+    registeration_fee: 80,
+    event_type_1: "Technical",
+    event_type_2: "Group",
+    head_name_1: "Alen T Binu",
+    head_phonenumber_1: "7558066581",
+    head_name_2: "Nithin J.K",
+    head_phonenumber_2: "8547433566",
+    description:
+"Get ready to unleash your creative genius in the upcoming Ideathon competition! With a dynamic theme unveiled on the competition day, it's your chance to tackle real-world problems head-on. Craft your innovative solutions and present them in a concise 10-slide PowerPoint presentation. The clock is ticking with a tight 3-hour submission, so don't miss the opportunity to showcase your problem-solving skill and make a real impact! Join us for a thrilling intellectual challenge in the Ideathon where your ideas shape the future."  },
+ 
+
+{
+    id: 18,
+    url: "/event5.jpg",
+    event_name: "Rampage Rush",
+    registeration_fee: 50,
+    event_type_1: "Technical",
+    event_type_2: "Single",
+    head_name_1: "Aben B John",
+    head_phonenumber_1: "9544453174",
+    head_name_2: "Aslam Salam",
+    head_phonenumber_2: "6235608419",
+    description:
+"Get ready to put your typing skills to the test in the ultimate keyboard showdown! Test your typing skills, reflexes, and accuracy as you race against the clock and fellow participants.Bring your A-game to the keyboard and showcase your lightning-fast typing prowess in this thrilling event. Prizes await the fastest fingers, so don't miss your chance to be crowned the typing champion of our fest!"  },
+  
+{
+    id: 12,
+    url: "/event6.jpg",
+    event_name: "Yenme Echoes",
+    registeration_fee: 50,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Naveen Prince Thomas",
+    head_phonenumber_1: "9188327753",
+    head_name_2: "Bency George",
+    head_phonenumber_2: "8750605951",
+    description:
+"Get ready for the sensational YENME ECHOES competition at Samyuktha 7.0, where vocal virtuosos will shine! This individual event welcomes both undergraduate and postgraduate students to showcase their singing prowess. With a strict 5-minute time limit and the exclusion of accompanying instruments and microphones, it's all about the pure voice. Participants will enchant in two riveting rounds, starting with the classical/melody segment using karaoke in the preliminaries and then rocking the stage with fast beats songs of their choice in the finals. Spot registrations are allowed, but remember to share your music tracks in mp3 format at least 45 minutes before the rounds begin. The judges' decisions will ultimately crown the Best Singer of Samyuktha7.0!"  },
+  
+{
+    id: 14,
+    url: "/event6.jpg",
+    event_name: "Binary brain busters",
+    registeration_fee: 50,
+    event_type_1: "Technical",
+    event_type_2: "Single",
+    head_name_1: "Jayaraj J Pillai",
+    head_phonenumber_1: "7510977535",
+    head_name_2: "Devika Rajeev",
+    head_phonenumber_2: "8281086599",
+    description:
+"Welcome to our technical quiz! We're excited to embark on a journey of knowledge and problem-solving with you. This quiz will test your technical expertise and critical thinking in various domains, from engineering to computer science. Whether you're a student, a tech enthusiast, or a professional, we hope you find this experience both enjoyable and enriching. Let's dive into the world of technology and explore the depths of your technicalknowledge!"  },
+  
+
+  {
+    id: 12,
+    url: "/event6.jpg",
+    event_name: "Classical Conclave",
+    registeration_fee: 50,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Gowri Anil Kumar",
+    head_phonenumber_1: "8590382681",
+    head_name_2: "Govind S Jayan",
+    head_phonenumber_2: "9496996050",
+    description:
+"uwhe"  },
+ 
+
+{
+  id: 15,
+  url: "/event6.jpg",
+  event_name: "Old School Kickoff",
+  registeration_fee: 50,
+  event_type_1: "Cultural",
+  event_type_2: "Group",
+  head_name_1: "Deron Babu",
+  head_phonenumber_1: "9947292171",
+  head_name_2: "Prince P Sali",
+  head_phonenumber_2: "7559963231",
+  description:
+"An electrifying spectacle that unites students and fans alike. The field is a vibrant battleground where talent, passion, and team spirit collide. The cheers of the crowd reverberate through the campus, creating an atmosphere of exhilaration and camaraderie. Students come together to celebrate their love for the sport, showcasing their skills and dedication. From thrilling matches to halftime entertainment, the college fest football event is a vibrant display of youthful enthusiasm and the perfect platform to witness the next generation of soccer stars in action" },
+
+
+{
+    id: 12,
+    url: "/event6.jpg",
+    event_name: "Polaroid Perspective",
+    registeration_fee: 50,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Ashwin Baburaj",
+    head_phonenumber_1: "8129294508",
+    head_name_2: "Amruth K Siby",
+    head_phonenumber_2: "9400164748",
+    description:
+"Join us for an enchanting journey through your lens at the POLAROID PERSPECTIVES photography event. Unleash your creativity with mobile cameras, DSLRs, SLRs, or digital cameras to capture the eerie and mystical. Remember, in this competition, editing and filtering."  },
+  
+  
+{
+    id: 20,
+    url: "/event6.jpg",
+    event_name: "binary arcade",
+    registeration_fee: 100,
+    event_type_1: "Technical",
+    event_type_2: "Single",
+    head_name_1: "Sneha Susan Rajesh",
+    head_phonenumber_1: "8547914287",
+    head_name_2: "Sneha Biju",
+    head_phonenumber_2: "7902385929",
+    description:
+"Join us at Codecrypt for an exhilarating coding event that will put your programming prowess to the test! This thrilling event is tailored for students passionate about programming and technology. Test your coding skills, problem-solving prowess, and creativity in a series of exciting challenges. Whether you're a novice or a seasoned coder, there's a place for you. Don't miss this opportunity to showcase your coding prowess and celebrate the spirit of innovation at Codecrypt!"  },
+  
+{
+    id: 12,
+    url: "/event6.jpg",
+    event_name: "back to the browser",
+    registeration_fee: 100,
+    event_type_1: "Technical",
+    event_type_2: "Single",
+    head_name_1: "Arjun J S",
+    head_phonenumber_1: "6238745782",
+    head_name_2: "Joseph Mel Joel",
+    head_phonenumber_2: "9946899977",
+    description:
+"EIEREE Interface isn't your typical web design competition. We're turning up the excitement by challenging you to think on your feet and adapt to unexpected creative twists. Imagine being presented with a surprise theme or constraint, and within a limited time, crafting a web design that dazzles. This contest isn't just about static websites; it's a dynamic showcase of your ability to innovate, improvise, and design web experiences that captivate. EIEREE Interface is where web design meets creativity in its most spontaneous form, and we invite you to be part of this thrilling designing adventure."  },
+ 
+{
+    id: 16,
+    url: "/event6.jpg",
+    event_name: "secret expedition",
+    registeration_fee: 100,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Pranav K Manoj",
+    head_phonenumber_1: "9048930995",
+    head_name_2: "Sneha Thomas",
+    head_phonenumber_2: "9633761142",
+    description:
+"Embark on an exhilarating journey through the mysterious and spooky world of 'Samyuktha' with our Treasure Hunt event. Prepare to put your problem-solving skills to the test as you follow cryptic clues, solve riddles, and uncover hidden treasures. It's a thrilling adventure where participants must work in teams, using their wit and creativity to navigate through a series of challenges."  },
+  
+{
+    id: 21,
+    url: "/event6.jpg",
+    event_name: "nova-lux of solaris",
+    registeration_fee: 50,
+    event_type_1: "Cultural",
+    event_type_2: "Single",
+    head_name_1: "Sarath S Nair",
+    head_phonenumber_1: "7560911621",
+    head_name_2: "Abel Mathew Abraham",
+    head_phonenumber_2: "8129317480",
+    description:
+"Elevate the Extraordinary: Star of Samyuktha is a platform where we shine a spotlight on individuals whose talents transcend traditional boundaries. From technical prowess to creative flair, we celebrate the extraordinary diversity of skills that make them true standouts. Join us for a one-of-a-kind showcase of talent that knows no limits!"  },
+
+];
+
+export default EventData;
+
