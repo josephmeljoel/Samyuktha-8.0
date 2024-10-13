148dcce50be55f38ec1b549de28c0fff1c5d92f3
paulprince24542 <paulprince2454@gmail.com>
2024-10-13 21:56:34 +0530

    Tracking script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Store } from "./context/store/store";
 import Providers from "./context/provider/provider";
 import { Suspense } from "react";
 import Loader from "./components/main/Loader";
+import Script from "next/script";
 
 const pressStart = Press_Start_2P({
   subsets: ["latin"],
@@ -26,6 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="5d4bfe57-260b-4d6c-af80-d54002c01dce"
+      />
       <body className={`bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
         <StarsCanvas />
 
